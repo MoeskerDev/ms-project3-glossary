d06cc818febd5f4f04d686d1189a6e77467892aa
MoeskerDev <mgmoesker@gmail.com>
2021-07-14 17:26:48 +0000

    Connected database mongo to collapsible component

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
   });
+
+
+var elem = document.querySelector('.collapsible.expandable');
+var instance = M.Collapsible.init(elem, {
+  accordion: false
+});
