aac687e70e01fa07aadae91d90a986ea733122a4
MoeskerDev <mgmoesker@gmail.com>
2021-07-14 21:57:16 +0000

    Adjusted add term form

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_terms")
-def get_terms():
+@app.route("/terms")
+def terms():
     terms = list(mongo.db.terms.find())
     return render_template("terms.html", terms=terms)
 
@@ -49,35 +49,33 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-        if request.method == "POST":
-            # check if username exists in db
-            existing_user = mongo.db.users.find_one(
-                {"username": request.form.get("username").lower()})
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
 
-            if existing_user:
-                # ensure hashed password matches user input
-                if check_password_hash(
-                    existing_user["password"], request.form.get("password")):
-                        session["user"] = request.form.get("username").lower()
-                        flash("Welcome, {}".format(
-                            request.form.get("username")))
-                        return redirect(url_for(
-                            "profile", username=session["user"]))
-                else:
-                    # invalid password match
-                    flash("Incorrect Username and/or Password")
-                    return redirect(url_for("login"))
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
             else:
-                # username doesn't exist
+                # invalid password match
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
-        return render_template("login.html")
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
-        # get the session user's username from db
+    # get the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
     return render_template("profile.html", username=username)
@@ -91,8 +89,20 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_term")
+@app.route("/add_term", methods=["GET", "POST"])
 def add_term():
+    if request.method == "POST":
+        term = {
+            "field_name": request.form.get("field_name"),
+            "term_name": request.form.get("term_name"),
+            "term_definition": request.form.get("term_definition"),
+            "created_by": session["user"],
+            "date_added": {}
+        }
+        mongo.db.terms.insert_one(term)
+        flash("Term Successfully Added")
+        return redirect(url_for("terms"))
+
     fields = mongo.db.fields.find().sort("field_name", 1)
     return render_template("add_term.html", fields=fields)
 
